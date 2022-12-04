f0b767cf48d781bb5c9b3761718828509f65cddd
Kent D <kent.daleng@nav.no>
2022-12-04 16:48:00 +0100

    map, open input in loop

--- 04/04.py
+++ 04/04.py
@@ -1,5 +1,4 @@
 import re
-l=[[int(j)for j in re.split('[,-]',x)]for x in open('data/input')]
 A=B=0
-for a,b,x,y in l:A+=(a-x)*(b-y)<1;B+=(b-x)*(a-y)<1
+for x in open('data/input'):a,b,x,y=map(int,re.split('[,-]',x));A+=(a-x)*(b-y)<1;B+=(b-x)*(a-y)<1
 print(A,B)
