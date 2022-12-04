3b3a8ab58657137f51b0ae0c516780e44d59b207
Kent D <kent.daleng@nav.no>
2022-12-04 12:06:52 +0100

    better checks

--- 04/04.py
+++ 04/04.py
@@ -1,5 +1,5 @@
 import re
 l=[[int(j)for j in re.split('[,-]',x)]for x in open('data/input')]
 A=B=0
-for a,x,b,y in l:B+=a<=y and x>=b;A+=(a<=b and x>=y)or(a>=b and x<=y)
+for a,x,b,y in l:A+=(a-b)*(x-y)<1;B+=(x-b)*(a-y)<1
 print(A,B)
